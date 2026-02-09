fe990a433db9fd53d8ca2fa26672a670cb59a7a3
Idansrur13 <office@hemy-hebrew.com>
2026-02-09 12:37:59 +0200

    Add files via upload

---
+++ js/game.js
@@ -0,0 +1,129 @@
+'use strict'
+
+const WALL = '&#8251;'
+const FOOD = '&middot;'
+const SUPERFOOD = '&#10022;'
+const SUPERGHOSTCOLOR = '#82ff72ff'
+const CHERRY = '🍒'
+const EMPTY = ' '
+
+const gGame = {
+  score: 0,
+  isOn: false,
+}
+var gBoard
+var countFood
+
+function init() {
+  const btnOver = document.querySelector('.gameOver')
+  countFood = 0
+  gGhosts = []
+
+  gBoard = buildBoard()
+  createPacman(gBoard)
+  createGhosts(gBoard)
+
+  renderBoard(gBoard, '.board-container')
+  btnOver.style.display = 'none'
+  gGame.isOn = true
+  countFood = counterFood()
+  setInterval(addCherry, 5000)
+}
+
+function buildBoard() {
+  const size = 10
+  const board = []
+
+  for (var i = 0; i < size; i++) {
+    board.push([])
+
+    for (var j = 0; j < size; j++) {
+      board[i][j] = FOOD
+
+      if (
+        i === 0 ||
+        i === size - 1 ||
+        j === 0 ||
+        j === size - 1 ||
+        (j === 3 && i > 4 && i < size - 2)
+      ) {
+        board[i][j] = WALL
+      }
+    }
+  }
+  board[1][1] = SUPERFOOD
+  board[8][8] = SUPERFOOD
+  board[8][1] = SUPERFOOD
+  board[1][8] = SUPERFOOD
+  return board
+}
+
+function updateScore(diff) {
+  // update model
+  gGame.score += diff
+  countFood--
+
+  if (countFood === 0) {
+    gameOver()
+  }
+
+  // update DOM
+  const elScore = document.querySelector('.score span')
+  elScore.innerText = gGame.score
+}
+
+function superFood() {
+  isSuperGhost = true
+
+  setTimeout(ghostReturn, 5000)
+}
+
+function addCherry() {
+  var cherryPos = {
+    i: getRandomIntInclusive(1, gBoard.length - 2),
+    j: getRandomIntInclusive(1, gBoard[0].length - 2),
+  }
+
+  if (
+    gBoard[cherryPos.i][cherryPos.j] === EMPTY ||
+    gBoard[cherryPos.i][cherryPos.j] === FOOD
+  ) {
+    renderCell(cherryPos, CHERRY)
+    gBoard[cherryPos.i][cherryPos.j] = CHERRY
+  }
+}
+
+function eatSuperGhost(nextPos) {
+  for (let i = 0; i < gGhosts.length; i++) {
+    const ghost = gGhosts[i]
+    if (ghost.pos.i === nextPos.i && ghost.pos.j === nextPos.j) {
+      var indxGhost = gGhosts.indexOf(ghost)
+      gGhosts.splice(indxGhost, 1)
+      setTimeout(createGhost, 4000)
+    }
+  }
+}
+
+function ghostReturn() {
+  isSuperGhost = false
+}
+
+function gameOver() {
+  const btnOver = document.querySelector('.gameOver')
+  console.log('Game Over')
+  gGame.isOn = false
+  btnOver.style.display = 'block'
+}
+
+function counterFood() {
+  var count = 0
+  for (let i = 0; i < gBoard.length; i++) {
+    for (let j = 0; j < gBoard[0].length; j++) {
+      const currCell = gBoard[i][j]
+      if (currCell === FOOD) {
+        count++
+      }
+    }
+  }
+  return count
+}
---
+++ js/ghost.js
@@ -0,0 +1,104 @@
+'use strict'
+
+const GHOST = '&#9865;'
+var gGhosts = []
+var isSuperGhost = true
+
+var gGhostsInterval
+
+function createGhosts() {
+  // TODO: Create 3 ghosts and an interval
+  for (var i = 0; i < 3; i++) {
+    createGhost()
+  }
+  gGhostsInterval = setInterval(moveGhosts, 1000)
+}
+
+function createGhost() {
+  // TODO: Create a ghost with arbitrary start pos & currCellContent
+  const ghost = {
+    pos: { i: 3, j: 3 },
+    currCellContent: EMPTY,
+    color: rundomColor(),
+  }
+  // TODO: Add the ghost to the ghosts array
+  gGhosts.push(ghost)
+
+  // TODO: Update the board
+  gBoard[ghost.pos.i][ghost.pos.j] = GHOST
+}
+
+function moveGhosts() {
+  // TODO: loop through ghosts
+  for (var i = 0; i < gGhosts.length; i++) {
+    moveGhost(gGhosts[i])
+  }
+}
+
+function moveGhost(ghost) {
+  // TODO: figure out moveDiff, nextPos, nextCell
+  const diff = getMoveDiff()
+  const nextPos = {
+    i: ghost.pos.i + diff.i,
+    j: ghost.pos.j + diff.j,
+  }
+  const nextCell = gBoard[nextPos.i][nextPos.j]
+
+  // TODO: return if cannot move
+  if (nextCell === WALL || nextCell === GHOST) return
+
+  // TODO: hitting a pacman? call gameOver
+  if (nextCell === PACMAN && !isSuperGhost) {
+    gameOver()
+    return
+  }
+
+  // TODO: moving from current pos:
+  // TODO: update the model (restore prev cell contents)
+  gBoard[ghost.pos.i][ghost.pos.j] = ghost.currCellContent
+
+  // TODO: update the DOM
+  renderCell(ghost.pos, ghost.currCellContent)
+
+  // TODO: Move the ghost to new pos:
+  // TODO: update the model (save cell contents so we can restore later)
+  ghost.pos = nextPos
+  ghost.currCellContent = nextCell
+
+  gBoard[ghost.pos.i][ghost.pos.j] = GHOST
+
+  // TODO: update the DOM
+  renderCell(ghost.pos, getGhostHTML(ghost))
+}
+
+function getMoveDiff() {
+  const randNum = getRandomIntInclusive(1, 4)
+
+  switch (randNum) {
+    case 1:
+      return { i: 0, j: 1 }
+    case 2:
+      return { i: 1, j: 0 }
+    case 3:
+      return { i: 0, j: -1 }
+    case 4:
+      return { i: -1, j: 0 }
+  }
+}
+
+function getGhostHTML(ghost) {
+  var color
+  if (isSuperGhost) {
+    color = SUPERGHOSTCOLOR
+  } else color = ghost.color
+  return `<span class="ghost" style="color:${color};">${GHOST}</span>`
+}
+
+function rundomColor() {
+  const hex = '0123456789ABCDEF'
+  let color = '#'
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)]
+  }
+  return color
+}
